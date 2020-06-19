e63e5a83066dcdfd0b5fefc450cddeb1e1fb2e13
Sam Brown <sam@sgb.io>
2020-06-19 21:27:17 +0100

    Add a primes calculator to demonstrate codehawk-bot

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const start = performance.now()
 
 const round = (num) => Math.round(num * 100) / 100
 
+// Adding this just to demonstrate codehawk-bot in a PR. It's not actually used.
+function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
 const readAllFiles = () => {
     const filePaths = []
     for (i = 0; i < 251; i++) {
